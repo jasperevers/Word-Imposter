67ea11c0fdc4a60fc242518eb17672beefefdadc
jasperevers <jasper.evers@student.hu.nl>
2025-09-15 14:54:26 +0200

    updated docker

--- backend/src/main/java/com/wordimposter/WordImposterApplication.java
+++ backend/src/main/java/com/wordimposter/WordImposterApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WordImposterBackendApplication {
+public class WordImposterApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WordImposterBackendApplication.class, args);
+        SpringApplication.run(WordImposterApplication.class, args);
     }
 }
